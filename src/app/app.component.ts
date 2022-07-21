54f4c9480189d117c5118b444102b2c514531ef7
Marcos <marcosmuller87@gmail.com>
2022-07-21 13:44:44 -0300

    Aplicativo desenvolvido com Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'piano-app';
+  dadoIzq = '../assets/img/dado1.png';
+  dadoDer = '../assets/img/dado2.png';
+  numero1 = 1;
+  numero2 = 2;
+
+  jogarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzq = '../assets/img/dado' + this.numero1 + '.png';
+    this.dadoDer = '../assets/img/dado' + this.numero2 + '.png';
+  }
 }
